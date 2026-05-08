2085f40da3f50634a328d54d596d1bbeb1e69cd0
Niyati-Rawal <niyati513@gmail.com>
2026-05-08 19:53:31 +0530

    changes

--- moodrecipeproj/settings.py
+++ moodrecipeproj/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --- Security ---
 SECRET_KEY = config("DJANGO_SECRET_KEY", default="dev-secret-change-in-prod")
 DEBUG = config("DEBUG", default=True, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
-
+ALLOWED_HOSTS = ['*']
 # --- Apps ---
 INSTALLED_APPS = [
     "django.contrib.admin",
